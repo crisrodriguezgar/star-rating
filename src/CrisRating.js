ffa5e7a2d84a31dba8c507de477089e988e54bfd
crisrodriguezgar <cristiro@DESKTOP-BCES715.localdomain>
2024-03-05 20:18:31 +0100

    update app

--- src/CrisRating.js
+++ src/CrisRating.js
@@ -1,33 +1,97 @@
-import { html, css, LitElement } from 'lit';
+import { LitElement, html, css } from 'lit';
 
 export class CrisRating extends LitElement {
   static styles = css`
     :host {
       display: block;
-      padding: 25px;
-      color: var(--cris-rating-text-color, #000);
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 30px;
+    }
+
+    .title {
+      font-weight: 400;
+      padding: 10px 0;
+    }
+
+    .stars {
+      display: flex;
+    }
+
+    .star {
+      width: 30px;
+      margin: 20px 10px;
+      cursor: pointer;
+    }
+
+    button {
+      border: none;
+      background-color: transparent;
+    }
+    textarea {
+      padding: 20px;
+      font-family: 'Montserrat', sans-serif;
+      &::placeholder {
+        font-family: 'Montserrat', sans-serif;
+      }
     }
   `;
 
   static properties = {
-    header: { type: String },
-    counter: { type: Number },
+    text: { type: String },
+    starsCounter: { type: Number },
+    rating: { type: Number },
   };
 
   constructor() {
     super();
-    this.header = 'Hey there';
-    this.counter = 5;
+    this.text = '¿Qué te ha parecido?';
+    this.starsCounter = 5;
+    this.rating = 0;
   }
 
-  __increment() {
-    this.counter += 1;
+  fillStar(index) {
+    this.filledStars = index + 1;
+    this.rating = index + 1;
+    this.requestUpdate();
+    console.log(this.rating);
   }
 
   render() {
+    const emptyStar = '../src/images/star.png';
+    const filledStar = '../src/images/star1.png';
+
+    const starsArray = Array.from({ length: this.starsCounter });
+
     return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <div class="container">
+        <span class="title">${this.text}</span>
+
+        <div class="stars">
+          ${starsArray.map(
+            (star, starIndex) =>
+              html`<button @click="${() => this.fillStar(starIndex)}">
+                <img
+                  class="star"
+                  src="${starIndex < this.filledStars ? filledStar : emptyStar}"
+                  alt="Estrella"
+                />
+              </button>`
+          )}
+        </div>
+
+        ${this.rating === 0
+          ? html``
+          : html`<textarea
+              placeholder="Déjanos saber tu opinión..."
+              cols="50"
+              rows="10"
+            ></textarea>`}
+      </div>
     `;
   }
 }
